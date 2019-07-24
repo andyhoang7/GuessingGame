a4a35b8243ff59f59f5f1a10ce756c79c29eb712
Thien Hoang <hoangkimthien@gmail.com>
2019-07-24 15:33:55 +0700

    Update 3

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,11 @@ let availableGuesses = 5
 document.getElementById("prompt").innerHTML = "Available Guesses: " + availableGuesses
 
 let guessTotal = 0
+const guesses = []
 
 function guessUserNumber () {
     guessTotal += 1
+
     if (availableGuesses === 1) {
         document.getElementById("prompt").innerHTML = "Sorry! Please try again :)"
         return
@@ -17,18 +19,31 @@ function guessUserNumber () {
     
     if (userGuess > pcNumber) {
         document.getElementById("inputValue").value = "";
-        document.getElementById("prompt").innerHTML = "Your number is too high: " + (availableGuesses -= 1)
+        document.getElementById("prompt").innerHTML = "Your number is too high: " + (availableGuesses -= 1) + " guesses left"
     }
     if (userGuess < pcNumber) {
         document.getElementById("inputValue").value = "";
-        document.getElementById("prompt").innerHTML = "Your number is too low: " + (availableGuesses -= 1)
+        document.getElementById("prompt").innerHTML = "Your number is too low: " + (availableGuesses -= 1) + " guesses left"
 
     }
     if (userGuess == pcNumber) {
         document.getElementById("inputValue").value = "";
-        document.getElementById("prompt").innerHTML = "Congratulations! You won in " + guessTotal + " guesses"
- 
+        if (guessTotal ==1 ) {
+            document.getElementById("prompt").innerHTML = "Congratulations! You won in 1 guess!"
+        } else {
+            document.getElementById("prompt").innerHTML = "Congratulations! You won in " + guessTotal  + " guesses"
+        }
     }
 
+    guesses.push(userGuess)
+
+    document.getElementById("guessesTracker").innerHTML = "";
+
+    guesses.map(guess => {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(guess);
+        node.appendChild(textnode);
+        document.getElementById("guessesTracker").appendChild(node);
+    })
 
 }
